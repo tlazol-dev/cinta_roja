029ec38bae91f40de6251fd09fe87f7355f623df
B3R307 <talzolteotlmeztli@gmail.com>
2019-07-04 22:01:44 -0500

    commit

--- 07_Express/app.js
+++ 07_Express/app.js
@@ -4,12 +4,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     console.log('hola en express');
-    res.status(200).send('servicio en express');
+    res.status(200).send('<h1>Bienvenido a Express</h1>');
 })
 
 app.listen(PORT, () => {
     console.log(`corriendo el puerto ${PORT}`);
 })
+
+
+//
